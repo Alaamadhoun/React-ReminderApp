050b21d7cb452c7153fce3bb2f63de2521efc34b
Alaamadhoun <alaafm@live.com>
2019-01-06 17:23:13 +0800

    Initial React Set up with Redux action creators

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">React Reminder</div>
+        <div className="form-inline">
+          <div className="form-group">
+            <input className="form-control" placeholder="Remind me to..." />
+          </div>
+          <button type="button" className="btn btn-success">
+            Add Reminder
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
