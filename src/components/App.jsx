c30200fbefecf1cb11029d93bb774c606228e241
Alaamadhoun <alaafm@live.com>
2019-01-07 20:57:00 +0800

    Time & Date Added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ""
+      text: "",
+      dueDate: ""
     };
   }
 
   addReminder() {
-    this.props.addReminder(this.state.text);
+    console.log("this.state.dueDate", this.state.dueDate);
+    this.props.addReminder(this.state.text, this.state.dueDate);
   }
 
   deleteReminder(id) {
@@ -25,7 +27,10 @@ class App extends Component {
         {reminders.map(reminder => {
           return (
             <li key={reminder.id} className="list-group-item">
-              <div className="list-item">{reminder.text}</div>
+              <div className="list-item">
+                <div> {reminder.text}</div>
+                <div>{reminder.dueDate}</div>
+              </div>
               <div
                 className="list-item delete-button"
                 onClick={() => this.deleteReminder(reminder.id)}
@@ -50,6 +55,11 @@ class App extends Component {
               className="form-control"
               placeholder="Remind me to..."
               onChange={event => this.setState({ text: event.target.value })}
+            />
+            <input
+              className="form-control"
+              type="datetime-local"
+              onChange={event => this.setState({ dueDate: event.target.value })}
             />
           </div>
           <button
